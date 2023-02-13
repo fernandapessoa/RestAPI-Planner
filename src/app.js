3f95a12f0ba44ffe3e00fb239b50abc1a0f7a874
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-02-12 23:09:40 -0400

    feat: middleware rota indefinida

--- src/app.js
+++ src/app.js
@@ -5,9 +5,19 @@ app.use(express.json());
 const userRouter = require("./routes/userRoutes");
 const eventRouter = require("./routes/eventRoutes");
 
+//MIDDLEWARE PARA ROTA NAO ENCONTRADA
+const undefinedRoute = (req, res) =>{
+    res.status(404).json({
+        status: 'failure',
+        message: "This route is not defined"
+    })
+};
+
 //ROUTES
 const baseRout = "/api/v1";
 app.use(`${baseRout}/users`, userRouter);
 app.use(`${baseRout}/events`, eventRouter);
+app.use('*', undefinedRoute);
 
 module.exports = app;
+ 
--- src/server.js
+++ src/server.js
@@ -5,3 +5,4 @@ const port = 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
